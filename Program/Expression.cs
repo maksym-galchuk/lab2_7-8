a62270b51c5c62a4d0a0cf0b0fe2d80185132138
Maksym Galchuk <maksymgalch@gmail.com>
2023-05-16 20:39:38 +0300

    Init

---
+++ Program/Expression.cs
@@ -0,0 +1,32 @@
+﻿namespace Main; 
+
+public class Expression {
+    private double a;
+    private double d;
+    private double c;
+    
+    private double result;
+
+
+    public Expression(double a, double d, double c) {
+        this.a = a;
+        this.d = d;
+        this.c = c;
+    }
+    
+    public double Calculate() {
+        double ln = Math.Log(1 - a / 4);
+        if (ln == 0) throw new Exception($"Division by zero exeption: Ln(1 - {a} / 4) = 0");
+        if ((1 - a / 4) <= 0) throw new Exception($"Invalid argument exeption: Ln(1 - {a} / 4) <= 0");
+        
+        result = (2 * c - d / 23) / ln;
+        // return result with 2 decimal places
+        return Math.Round(result, 2);
+    }
+    
+    public string GetExpression() => $"(2 * {c} - {d} / 23) / (Ln(1 - {a} / 4))";
+    
+    public override string ToString() {
+        return $"Expression: {GetExpression()}\nResult: {Calculate()}";
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using Main;
+while (true) {
+    Console.WriteLine("Enter a range of arguments for expression in the form of 'start end' or 'exit' to exit the program");
+    string input = Console.ReadLine();
+    string[] range = input.Split(' ');
+    
+    if (input == "exit") break;
+    if (!(input.Replace(" ", "").Replace("-", "").All(char.IsDigit)) || range.Length != 2) {
+        Console.WriteLine("Invalid input");
+        continue;
+    }
+    
+    int start = Int32.Parse(range[0]);
+    int end = Int32.Parse(range[1]);
+    
+    Expression[] expressions = new Expression[end - start];
+    
+    for (int i = start; i < end; i++) {
+        Expression expression = new Expression(i, i*i + 2, i*2 - 5);
+        expressions[i - start] = expression;
+        
+        Console.WriteLine($"Argument: a = {i}, d = {i*i + 2}, c = {i*2 - 5}");
+        try {
+            Console.WriteLine(expression + "\n");
+        } catch (Exception e) {
+            
+            Console.WriteLine($"Expression: {expression.GetExpression()}");
+            Console.WriteLine(e.Message + "\n");
+        }
+    }
+
+}
